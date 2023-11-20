53446ce7e026d905085568a58f2a9b3543e52339
justaregularchalo <gonzalo.lara.caceres@gmail.com>
2023-11-20 18:24:49 +0100

    done

---
+++ src/components/Message.jsx
@@ -0,0 +1,22 @@
+function Message (props) {
+
+    return (
+
+        <div>
+
+        <p className="message">
+       {props.message}
+        </p>
+          
+        </div>
+
+
+    )
+
+
+
+
+}
+
+
+export default Message;
---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,20 @@
+function TimeStamps (props) {
+
+    return (
+
+        <div>
+
+        <span className="timestamp">{props.timestamp}</span>
+          
+        </div>
+
+
+    )
+
+
+
+
+}
+
+
+export default TimeStamps;
